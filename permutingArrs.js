4ff18fb8cd8dd0122b9750c0977a1e8b4108c999
kristou799@gmail.com <kristiyan.svetoslavov.petkov@gmail.com>
2022-08-25 12:49:55 +0300

    task 8

--- permutingArrs.js
+++ permutingArrs.js
@@ -1,19 +1,22 @@
 function twoArrays(k, A, B) {
   // Write your code here
 
+  let sum = [];
+
   let sortedA = A.sort();
-  let sortedB = B.sort();
+  let sortedB = B.sort((a, b) => b - a);
 
-  let aIndex = 0;
-  let bIndex = sortedB.length - 1;
-
-  while (aIndex < sortedA.length && bIndex >= 0) {
-    if (sortedA[aIndex] + sortedB[bIndex] < k) return "NO";
-    aIndex++;
-    bIndex--;
+  for (let i = 0; i <= sortedA.length - 1; i++) {
+    sum.push(sortedA[i] + sortedB[i]);
   }
 
-  return "YES";
+  let everyIsBiggerThanK = sum.every((n) => n >= k);
+
+  if (everyIsBiggerThanK) {
+    return "YES";
+  } else {
+    return "NO";
+  }
 }
 
 // twoArrays(10, [2, 1, 3], [7, 8, 9]);
